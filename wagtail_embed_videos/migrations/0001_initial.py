d2ac5ab73f447e6667c02e12db3b3723dd0edb53
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2021-01-09 19:18:07 -0400

    fix upgrading wagtail 1.7 issue https://github.com/wagtail/wagtail/issues/5067#

--- wagtail_embed_videos/migrations/0001_initial.py
+++ wagtail_embed_videos/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
                 ('url', embed_video.fields.EmbedVideoField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('tags', taggit.managers.TaggableManager(blank=True, help_text=None, through='taggit.TaggedItem', to='taggit.Tag', verbose_name='Tags')),
-                ('thumbnail', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='wagtailimages.Image', verbose_name='Thumbnail')),
+                ('thumbnail', models.IntegerField(blank=True, null=True),
                 ('uploaded_by_user', models.ForeignKey(blank=True, editable=False, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
